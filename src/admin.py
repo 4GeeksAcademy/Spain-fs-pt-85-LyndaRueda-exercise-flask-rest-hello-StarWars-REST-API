fc16360b686ef73eb359aba70496cc6262961537
Lynda <155568879+LZRUEDADELGADO@users.noreply.github.com>
2025-01-27 17:43:26 +0000

    relaciones muchos a muchos terminada

--- src/admin.py
+++ src/admin.py
@@ -1,16 +1,22 @@
 import os
 from flask_admin import Admin
-from models import db, User
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.form import SecureForm
+from wtforms import PasswordField
+from wtforms.validators import InputRequired
+from models import db, User, Character, Episode, Location, Favorite
+
+class UserAdmin(ModelView):
+    form_base_class = SecureForm
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    admin = Admin(app, name='Rick and Morty Admin', template_mode='bootstrap3')
 
-    
-    # Add your models here, for example this is how we add a the User model to the admin
+    # Modelos al panel de adminin con vistas personalizadas para c/u
     admin.add_view(ModelView(User, db.session))
-
-    # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
+    admin.add_view(ModelView(Character, db.session))
+    admin.add_view(ModelView(Episode, db.session))
+    admin.add_view(ModelView(Location, db.session))
+    admin.add_view(ModelView(Favorite, db.session))
